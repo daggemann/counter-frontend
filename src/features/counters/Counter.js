3f4a37225810bf5e4b24c5d8edaa8e757bbddc1c
daggemann <dag.stromsvag@gmail.com>
2018-03-11 18:55:59 +0100

    Refactoring of counter-container

--- src/features/counters/Counter.js
+++ src/features/counters/Counter.js
@@ -1,8 +1,166 @@
-export default class Counter {
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL + '/counters';
+
+export class Counter {
 
     constructor(id, title, count) {
-        this.id = id;
-        this.title = title;
-        this.count = count;
+        this._id = id;
+        this._title = title;
+        this._count = count;
     }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    get count() {
+        return this._count;
+    }
+
+    set count(count) {
+        this._count = count;
+    }
+
+    increment() {
+        return new Promise( (resolve, reject) => {
+            axios.post(API_URL + `/${this._id}/increment`, {})
+                .then(res => {
+                    const counters = res.data.map( (counter) => {
+                        return new Counter(counter.id, counter.title, counter.count);
+                    });
+                    resolve(counters);
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+
+    delete() {
+        return new Promise( (resolve, reject) => {
+            axios.delete(API_URL + `/${this._id}`)
+                .then(res => {
+                    const counters = res.data.map( (counter) => {
+                        return new Counter(counter.id, counter.title, counter.count);
+                    });
+                    resolve(counters);
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+
+    decrement() {
+        return new Promise( (resolve, reject) => {
+            axios.post(API_URL + `/${this._id}/decrement`, {})
+                .then(res => {
+                    const counters = res.data.map( (counter) => {
+                        return new Counter(counter.id, counter.title, counter.count);
+                    });
+                    resolve(counters);
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+}
+
+
+export function calculateTotalCount(counters) {
+    let totalCount = 0;
+    counters.forEach((counter) => {
+        totalCount += counter.count;
+    });
+    return totalCount;
+}
+
+export function getAllCounters() {
+    return new Promise((resolve, reject) => {
+        axios.get(API_URL)
+            .then(res => {
+                const counters = res.data.map( (counter) => {
+                    return new Counter(counter.id, counter.title, counter.count);
+                });
+                resolve(counters);
+            })
+            .catch(err => {
+                reject(err)
+            })
+
+    })
+}
+
+export function addNewCounter(title) {
+    return new Promise( (resolve, reject) => {
+        axios.post(API_URL, {title: title})
+            .then(res => {
+                const counters = res.data.map( (counter) => {
+                    return new Counter(counter.id, counter.title, counter.count);
+                });
+                resolve(counters);
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+}
+
+export function incrementCounter(id) {
+    return new Promise( (resolve, reject) => {
+        axios.post(API_URL + `/${id}/increment`, {})
+            .then(res => {
+                const counters = res.data.map( (counter) => {
+                    return new Counter(counter.id, counter.title, counter.count);
+                });
+                resolve(counters);
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+}
+
+export function decrementCounter(id) {
+    return new Promise( (resolve, reject) => {
+        axios.post(API_URL + `/${id}/decrement`, {})
+            .then(res => {
+                const counters = res.data.map( (counter) => {
+                    return new Counter(counter.id, counter.title, counter.count);
+                });
+                resolve(counters);
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
+}
+
+export function deleteCounter(id) {
+    return new Promise( (resolve, reject) => {
+        axios.delete(API_URL + `/${id}`)
+            .then(res => {
+                const counters = res.data.map( (counter) => {
+                    return new Counter(counter.id, counter.title, counter.count);
+                });
+                resolve(counters);
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
 }
